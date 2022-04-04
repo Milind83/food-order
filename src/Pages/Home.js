c39f45668153f84aeee8b03b05e73f1d9150dc22
Milind <milindkhariwale83@gamil.com>
2022-04-04 18:27:51 +0530

    dvscs

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
-import food from './assets/food.jpg'
-
+import food from "../ ../images/food.jpg"
 function Home() {
   return (
     <div className="Home" style={{backgraundImage: `url(${food})`}}>
